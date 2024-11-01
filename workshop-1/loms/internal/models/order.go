8145e92c3fd5ddd3aec116cd98f5f832451825d0
just-a-developer-man <167205303+just-a-developer-man@users.noreply.github.com>
2024-11-01 11:41:43 +0300

    Redesigned models according to task

--- workshop-1/loms/internal/models/order.go
+++ workshop-1/loms/internal/models/order.go
@@ -1,29 +1,14 @@
 package models
 
-import (
-	"time"
-
-	"github.com/google/uuid"
-)
-
 // Order - заказ
 type Order struct {
-	UUID              uuid.UUID       // UUID заказа
-	UserID            UserID          // ID пользователя (чей заказ)
-	Items             []ItemOrderInfo // Информация о составе заказа
-	DeliveryOrderInfo                 // Информация о доставке
-	/* ... */
-}
-
-// DeliveryOrderInfo - информация о доставке заказа
-type DeliveryOrderInfo struct {
-	DeliveryVariantID DeliveryVariantID
-	DeliveryDate      time.Time
+	ID     OrderID         // ID заказа
+	UserID UserID          // ID пользователя (чей заказ)
+	Items  []ItemOrderInfo // Информация о составе заказа
 }
 
 // ItemOrderInfo - информация о составе заказа
 type ItemOrderInfo struct {
-	SKU         SKU         // SKU
-	Quantity    uint16      // количество SKU
-	WarehouseID WarehouseID // с какого склада будет браться сток
+	SKU      uint32 // SKU
+	Quantity uint16 // количество SKU
 }
--- workshop-1/loms/internal/models/types.go
+++ workshop-1/loms/internal/models/types.go
@@ -3,11 +3,5 @@ package models
 // UserID - тип id пользователя
 type UserID int64
 
-// SKUID - тип id товарной еденицы
-type SKUID int64
-
-// WarehouseID - тип id склада
-type WarehouseID int64
-
-// DeliveryVariantID - тип id способа доставки
-type DeliveryVariantID int64
+// OrderID - тип id заказа
+type OrderID int64
