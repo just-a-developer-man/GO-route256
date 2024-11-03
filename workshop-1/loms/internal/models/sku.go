5faf2d69bff2dc2a72026a5e3017937e5d57c299
just-a-developer-man <167205303+just-a-developer-man@users.noreply.github.com>
2024-11-03 16:25:38 +0300

    Deleted unnecessary types

--- workshop-1/loms/internal/models/sku.go
+++
@@ -1,9 +0,0 @@
-package models
-
-// SKU - единица складского учёта
-type SKU struct {
-	ID    SKUID  `json:"item_id" ,db:"id"`     // ID товарной единицы
-	Name  string `json:"item_name" ,db:"name"` // Название товарной единицы
-	Price uint64 `json:"item_price" ,db:"-"`   // Цена одной товарной единицы
-	/* ... */
-}
