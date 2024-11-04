0f85e5b10417ce142d6a5ad3fc04154c7a0d03ac
just-a-developer-man <167205303+just-a-developer-man@users.noreply.github.com>
2024-11-04 18:40:16 +0300

    Wrap handler with method check middlware

--- workshop-1/loms/internal/controller/http/router.go
+++ workshop-1/loms/internal/controller/http/router.go
@@ -2,6 +2,8 @@ package controller_http
 
 import (
 	"net/http"
+
+	"github.com/just-a-developer-man/GO-route256/workshop-1/loms/internal/controller/http/middleware"
 )
 
 // NewRouter - returns http.Handler
@@ -10,7 +12,7 @@ func (c *Controller) NewRouter() http.Handler {
 	mux := http.NewServeMux()
 
 	// Note: You can add here custom middleware too
-	mux.HandleFunc("/v1/order/create", c.CreateOrderHandler)
+	mux.HandleFunc("/v1/order/create", middleware.CheckMethodPostMiddleware(c.CreateOrderHandler))
 
 	return mux
 }
