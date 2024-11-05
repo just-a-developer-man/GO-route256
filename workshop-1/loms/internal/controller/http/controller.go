fc026385765d100ee7ba0fbb1159e7cfcc13ade1
just-a-developer-man <167205303+just-a-developer-man@users.noreply.github.com>
2024-11-05 12:42:49 +0300

    Interface initialization where it is used

--- workshop-1/loms/internal/controller/http/controller.go
+++ workshop-1/loms/internal/controller/http/controller.go
@@ -1,9 +1,20 @@
 package controller_http
 
-import "github.com/just-a-developer-man/GO-route256/workshop-1/loms/internal/usecase"
+import (
+	"context"
+
+	"github.com/just-a-developer-man/GO-route256/workshop-1/loms/internal/dto"
+	"github.com/just-a-developer-man/GO-route256/workshop-1/loms/internal/models"
+)
+
+type OrderManagementSystem interface {
+	CreateOrder(ctx context.Context, userID models.UserID, info dto.CreateOrderInfo) (models.OrderID, error)
+	OrderByID(ctx context.Context, orderID models.OrderID) (dto.OrderInfo, error)
+	/* ... */
+}
 
 type Usecases struct {
-	usecase.OrderManagementSystem // OMS interface
+	OrderManagementSystem // OMS interface
 }
 
 // Controller - is controller/delivery layer
