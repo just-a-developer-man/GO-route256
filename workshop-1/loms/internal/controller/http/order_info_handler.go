8bee477186a0933e6ca63fbe8f307152adb05273
just-a-developer-man <167205303+just-a-developer-man@users.noreply.github.com>
2024-11-05 12:47:01 +0300

    Hanlder logic impl for order info

--- workshop-1/loms/internal/controller/http/order_info_handler.go
+++ workshop-1/loms/internal/controller/http/order_info_handler.go
@@ -6,6 +6,9 @@ import (
 	"fmt"
 	"log/slog"
 	"net/http"
+
+	"github.com/just-a-developer-man/GO-route256/workshop-1/loms/internal/dto"
+	"github.com/just-a-developer-man/GO-route256/workshop-1/loms/internal/models"
 )
 
 type (
@@ -37,15 +40,30 @@ func (c *Controller) OrderInfoHandler(w http.ResponseWriter, r *http.Request) {
 	var req OrderInfoRequest
 	if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
 		slog.ErrorContext(ctx, "request body decoding failed", "err", err)
-		http.Error(w, err.Error(), http.StatusBadRequest)
+		http.Error(w, "", http.StatusBadRequest)
 		return
 	}
 
 	if err := validateOrderInfoRequest(req, validateOrderID); err != nil {
 		slog.ErrorContext(ctx, "request validation failed", "err", err)
-		http.Error(w, err.Error(), http.StatusBadRequest)
+		http.Error(w, "", http.StatusBadRequest)
 		return
 	}
+
+	orderInfo, err := c.Usecases.OrderManagementSystem.OrderByID(ctx, models.OrderID(req.OrderID))
+	if err != nil {
+		slog.ErrorContext(ctx, "get order by id failed", "err", err)
+		http.Error(w, "", http.StatusInternalServerError)
+		return
+	}
+
+	response := orderInfoToResponse(orderInfo)
+	if err := json.NewEncoder(w).Encode(response); err != nil {
+		slog.ErrorContext(ctx, "cannot write encode or write response", "err", err)
+		http.Error(w, "", http.StatusInternalServerError)
+		return
+	}
+	w.WriteHeader(http.StatusOK)
 }
 
 func validateOrderInfoRequest(req OrderInfoRequest, funcs ...validateOrderInfoRequestFunc) error {
@@ -63,4 +81,21 @@ func validateOrderID(req OrderInfoRequest) error {
 		return errors.New("bad order ID")
 	}
 	return nil
+}
+
+func orderInfoToResponse(info dto.OrderInfo) OrderInfoResponse {
+	items := make([]ItemInfo, 0, len(info.Items))
+
+	for _, item := range info.Items {
+		items = append(items, ItemInfo{
+			SKU:      item.SKU,
+			Quantity: item.Quantity,
+		})
+	}
+
+	return OrderInfoResponse{
+		Status: orderStatus(info.OrderStatus),
+		UserID: int64(info.UserID),
+		Items:  items,
+	}
 }
